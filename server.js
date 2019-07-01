4eb72e6f5fcdfad359c20d130fc4fe2864aef28d
Gregory Artisi <gregoryartisi@hotmail.com>
2019-07-01 17:39:41 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');//libraiie express.js
+var bodyParser = require("body-parser");
+var session = require('express-session');
+var url = require('url');//appel à la librairie url qui permet de créer des adresses
+var querystring = require('querystring');//permet de parser les paramètres de requêtes
+
+var app = express();
+// utiliser le module de parsing pour créer une variable "body automatiquement"
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+
+
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil');
+});
+//on recoit un post
+app.post('/todo/add', function(req, res) {
+	
+	var dodo = req.body.todo;
+	if(req.session.toDoList){
+		
+		req.session.toDoList.push(dodo);
+		
+	}else{
+		req.session.toDoList=new Array();//array stocké dans une variabled de session, chaque user aura donc une liste différente
+		req.session.toDoList.push(dodo);
+	}
+	res.redirect("/todo/");
+	
+	/*if(req.session.toDoList.length > 0){
+		console.log(req.session.toDoList);
+		res.render('todo.ejs',{todo:req.session.toDoList});
+	}else{
+		res.render('todo.ejs');
+    }*/
+});
+
+app.get('/todo', function(req, res) {
+	if(req.session.toDoList && req.session.toDoList.length > 0){
+		res.render('todo.ejs',{todo:req.session.toDoList});
+	}else{
+		req.session.toDoList;
+		res.render('todo.ejs');
+    }
+});
+
+app.get('/todo/remove/:item', function(req, res) {
+	//var params = querystring.parse(url.parse(req.url).query);
+	var index = req.params.item;
+	if (index > -1) {
+		req.session.toDoList.splice(index, 1);
+	}
+	res.redirect("/todo/");
+	/*if(req.session.toDoList && req.session.toDoList.length > 0){
+		res.render('todo.ejs',{todo:req.session.toDoList});
+	}else{
+		req.session.toDoList;
+		res.render('todo.ejs');
+    }*/
+});
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8080);
