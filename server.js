e57970b399b262a76de292225e8c99e9b8af31da
Gregory Artisi <gregoryartisi@hotmail.com>
2019-07-02 21:09:40 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');//libraiie express.js
+var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
 var session = require('express-session');
 var url = require('url');//appel à la librairie url qui permet de créer des adresses
 var querystring = require('querystring');//permet de parser les paramètres de requêtes
+var http = require('http');//node http lib
+
 
 var app = express();
 // utiliser le module de parsing pour créer une variable "body automatiquement"
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 
 
@@ -29,12 +33,7 @@ app.post('/todo/add', function(req, res) {
 	}
 	res.redirect("/todo/");
 	
-	/*if(req.session.toDoList.length > 0){
-		console.log(req.session.toDoList);
-		res.render('todo.ejs',{todo:req.session.toDoList});
-	}else{
-		res.render('todo.ejs');
-    }*/
+	
 });
 
 app.get('/todo', function(req, res) {
@@ -53,13 +52,11 @@ app.get('/todo/remove/:item', function(req, res) {
 		req.session.toDoList.splice(index, 1);
 	}
 	res.redirect("/todo/");
-	/*if(req.session.toDoList && req.session.toDoList.length > 0){
-		res.render('todo.ejs',{todo:req.session.toDoList});
-	}else{
-		req.session.toDoList;
-		res.render('todo.ejs');
-    }*/
+	
 });
+
+
+
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
