01e3c0e857162dff96b7dafd6de4ad08e1297b60
Gregory Artisi <gregoryartisi@hotmail.com>
2019-07-06 18:21:17 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');//libraiie express.js
+var jquery = require('jquery');
 var cookieParser = require('cookie-parser');
 var bodyParser = require("body-parser");
 var session = require('express-session');
@@ -8,58 +9,68 @@ var http = require('http');//node http lib
 
 
 var app = express();
-// utiliser le module de parsing pour créer une variable "body automatiquement"
+var server = require('http').createServer(app);//création d'un server à partir de l'app express
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+//todo list
+var todoList= new Map();
+var sizeList =0;
+
+// utiliser le module de parsing pour créer une variable "body automatiquement", cette variable permet de récupérer les paramètres de requête
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
-
+app.use(express.static('public'))
 
 
 app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.send('Vous êtes à l\'accueil');
 });
-//on recoit un post
-app.post('/todo/add', function(req, res) {
-	
-	var dodo = req.body.todo;
-	if(req.session.toDoList){
-		
-		req.session.toDoList.push(dodo);
-		
-	}else{
-		req.session.toDoList=new Array();//array stocké dans une variabled de session, chaque user aura donc une liste différente
-		req.session.toDoList.push(dodo);
-	}
-	res.redirect("/todo/");
-	
-	
-});
 
 app.get('/todo', function(req, res) {
-	if(req.session.toDoList && req.session.toDoList.length > 0){
-		res.render('todo.ejs',{todo:req.session.toDoList});
-	}else{
-		req.session.toDoList;
-		res.render('todo.ejs');
-    }
-});
-
-app.get('/todo/remove/:item', function(req, res) {
-	//var params = querystring.parse(url.parse(req.url).query);
-	var index = req.params.item;
-	if (index > -1) {
-		req.session.toDoList.splice(index, 1);
-	}
-	res.redirect("/todo/");
+	
+		res.render('todo.ejs',{todo:todoList});
 	
 });
 
 
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log("conexion d'un client");
+	
+	/*edit d'une task*/
+	socket.on('edit', function (task) {
+        todoList[task.task]=task.textT;
+		socket.emit('todoList',todoList);
+		socket.broadcast.emit('todoList',todoList); // Send message to everyone BUT sender
+		
+    });	
+	/*ajout d'une task renvoyé vers tous les clients*/
+	socket.on('add', function (task) {
+        console.log(task);
+		todoList.set(sizeList,task);
+		num = sizeList;
+		 
+		console.log(task+"  "+num);
+		socket.emit('task',{'texte':task,'num':num});
+		socket.broadcast.emit('task',{'texte':task,'num':num}); // Send message to everyone BUT sender
+		sizeList ++;
+    });	
+	
+	socket.on('remove', function (task) {
+        console.log(task);
+		todoList.delete(task);
+		
+		socket.emit('del',task);
+		socket.broadcast.emit('del',task); // Send message to everyone BUT sender
+		
+    });	
+});
 
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !');
 });
 
-app.listen(8080);
+server.listen(8080, "127.0.0.1");
